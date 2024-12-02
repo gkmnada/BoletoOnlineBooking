9a8ef8163d5f963629fb82af7919ebbcf247c75d
gkmnada <gkmenada@hotmail.com>
2024-12-02 18:24:56 +0300

    Added MVC layer and business layer and DTO layer for Frontends

---
+++ src/Frontends/Boleto.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Boleto.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
